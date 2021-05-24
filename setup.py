dfc041dc4a31d4833bce7d304c61a078ceb994d9
connor sanderford <connorsanderford@gmail.com>
2021-05-23 19:19:38 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+REQUIRED = [
+    'numpy'
+]
+
+with open('README.md', 'r') as fh:
+    LONG_DESCRIPTION = fh.read()
+    setuptools.setup(
+        name = 'morphontogeny',
+        version = '0.0.1',
+        author = 'BioProtean Labs',
+        description = 'Analysis scripts for morphometry and spacial transcriptomic data.',
+        long_description = LONG_DESCRIPTION,
+        long_description_content_type ='text/markdown',
+        url = 'http://bioprotean.org/',
+        packages = setuptools.find_packages(),
+        python_requires = '>=3.5',
+        install_requires = REQUIRED,
+        classifiers = ["Programming Language :: Python :: 3",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Operating System :: OS Independent"]
+    )
